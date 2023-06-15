0912610e7307eb03da1c9f80ed289c504b21cbcd
Mikhail Tsoy <mixail0071@yandex.ru>
2023-06-15 10:58:22 +0200

    wip

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+
+function App() {
+    return (
+        <div className="App">
+            <form>
+                <Input />
+                <Input />
+                <Button />
+            </form>
+        </div>
+    );
+}
+
+export default App;
